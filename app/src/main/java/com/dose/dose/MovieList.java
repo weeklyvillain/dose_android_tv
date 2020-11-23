5768f2d11746eccd7c89620c558e56142c74fc24
Filip Eriksson <f.eriksson.21.fe@gmail.com>
2020-11-23 15:52:12 +0100

    Initial layout started, using some dummy data

---
+++ app/src/main/java/com/dose/dose/MovieList.java
@@ -0,0 +1,98 @@
+package com.dose.dose;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class MovieList {
+    public static final String MOVIE_CATEGORY[] = {
+            "Category Zero",
+            "Category One",
+            "Category Two",
+            "Category Three",
+            "Category Four",
+            "Category Five",
+    };
+
+    private static List<Movie> list;
+    private static long count = 0;
+
+    public static List<Movie> getList() {
+        if (list == null) {
+            list = setupMovies();
+        }
+        return list;
+    }
+
+    public static List<Movie> setupMovies() {
+        list = new ArrayList<>();
+        String title[] = {
+                "Back To The Future",
+                "Iron Man",
+                "The Flash",
+                "Harry Potter",
+                "Iron Man 2"
+        };
+
+        String description = "Fusce id nisi turpis. Praesent viverra bibendum semper. "
+                + "Donec tristique, orci sed semper lacinia, quam erat rhoncus massa, non congue tellus est "
+                + "quis tellus. Sed mollis orci venenatis quam scelerisque accumsan. Curabitur a massa sit "
+                + "amet mi accumsan mollis sed et magna. Vivamus sed aliquam risus. Nulla eget dolor in elit "
+                + "facilisis mattis. Ut aliquet luctus lacus. Phasellus nec commodo erat. Praesent tempus id "
+                + "lectus ac scelerisque. Maecenas pretium cursus lectus id volutpat.";
+        String studio[] = {
+                "116 min", "126 min", "No Time", "111 min", "133 min"
+        };
+        String videoUrl[] = {
+                "http://commondatastorage.googleapis.com/android-tv/Sample%20videos/Zeitgeist/Zeitgeist%202010_%20Year%20in%20Review.mp4",
+                "https://www.mp4point.com/downloads/ede99d243af0.mp4", // IRON MAN TRAILER
+                "http://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Gmail%20Blue.mp4",
+                "http://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Google%20Fiber%20to%20the%20Pole.mp4",
+                "http://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Google%20Nose.mp4"
+        };
+        String bgImageUrl[] = {
+                "http://commondatastorage.googleapis.com/android-tv/Sample%20videos/Zeitgeist/Zeitgeist%202010_%20Year%20in%20Review/bg.jpg",
+                "http://commondatastorage.googleapis.com/android-tv/Sample%20videos/Demo%20Slam/Google%20Demo%20Slam_%2020ft%20Search/bg.jpg",
+                "http://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Gmail%20Blue/bg.jpg",
+                "http://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Google%20Fiber%20to%20the%20Pole/bg.jpg",
+                "http://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Google%20Nose/bg.jpg",
+        };
+        String cardImageUrl[] = {
+                "https://ae01.alicdn.com/kf/HTB1FovjaYr1gK0jSZR0q6zP8XXa2/7x5ft-Back-to-The-Future-Car-Race-Track-Clouds-Custom-Photo-Studio-Background-Backdrop-Vinyl-220cm.jpg",
+                "https://comicattractions.com/wp-content/uploads/2019/03/iron-man-backdrop.jpg",
+                "https://backgrounds4k.net/wp-content/uploads/2016/01/The-Flash-backgrounds-ultra-hd.jpg",
+                "https://archziner.com/wp-content/uploads/2020/02/poster-for-deathly-hallows-part-two-movie-wallpaper-harry-potter-battle-of-hogwarts-450x280.jpg",
+                "https://wallpaperaccess.com/full/464869.jpg"
+        };
+
+        for (int index = 0; index < title.length; index++) {
+            list.add(
+                    buildMovieInfo(
+                            title[index],
+                            description,
+                            studio[index],
+                            videoUrl[index],
+                            cardImageUrl[index],
+                            bgImageUrl[index]));
+        }
+
+        return list;
+    }
+
+    private static Movie buildMovieInfo(
+            String title,
+            String description,
+            String studio,
+            String videoUrl,
+            String cardImageUrl,
+            String backgroundImageUrl) {
+        Movie movie = new Movie();
+        movie.setId(count++);
+        movie.setTitle(title);
+        movie.setDescription(description);
+        movie.setStudio(studio);
+        movie.setCardImageUrl(cardImageUrl);
+        movie.setBackgroundImageUrl(backgroundImageUrl);
+        movie.setVideoUrl(videoUrl);
+        return movie;
+    }
+}
